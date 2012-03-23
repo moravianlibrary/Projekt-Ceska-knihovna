97dcd7a58e447677eae32d0280d898596f80fcac
Karel Vasicek <karel.vasicek@webstep.net>
2012-03-23 08:49:33 +0100

    Oprava nefunkcniho ukladani objednavek pri ajax strankovani na jine nez prvni strance

--- ck/protected/views/libOrder/_sheet_item.php
+++ ck/protected/views/libOrder/_sheet_item.php
@@ -1,12 +1,10 @@
 <div class="view" id="book_<?echo $data->id?>">
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('publisher_id')); ?>:</b>
-	<?php //echo CHtml::link(CHtml::encode($data->name), '#', array('onclick'=>CHtml::ajax(array('url'=>url('publisher/view',array('id'=>$data->publisher_id)), 'success'=>'function(data){$("#publisher-juidialog-content").html(data);$("#publisher-juidialog").dialog("option", "modal", false).dialog("open");return false;}')))); ?>
 	<span><?php echo CHtml::encode($data->name); ?></span>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('title')); ?>:</b>
-	<?php //echo CHtml::link(CHtml::encode($data->title), '#', array('onclick'=>CHtml::ajax(array('url'=>url('book/view',array('id'=>$data->id)), 'success'=>'function(data){$("#book-juidialog-content").html(data);$("#book-juidialog").dialog("option", "modal", false).dialog("open");return false;}')))); ?>
 	<span><?php echo CHtml::encode($data->title); ?></span>
 	<br />
 
@@ -26,7 +24,7 @@
 	<span><?php echo CHtml::encode($data->annotation); ?></span>
 	<br />
 	<br />
-
+	
 	<b><?php echo t('Order'); ?>:</b>
 	<table>
 		<tr>
@@ -44,7 +42,7 @@
 					<?echo CHtml::hiddenField('LibOrder[book_id]', $data->id)?>
 					<?echo CHtml::hiddenField('LibOrder[type]', 'B')?>
 					<?php echo CHtml::encode($data->basic->count); ?> <?echo CHtml::textField('LibOrder[count]', $data->basic->count, array('size'=>'5')); ?> <?echo t('pcs')?>
-					<?php echo CHtml::ajaxSubmitButton(($data->basic->count ? t('Change Order') : t('Add To Order')), array('saveSheet', 'id'=>$data->basic->id), array('type'=>'POST', 'dataType'=>'json', 'beforeSend'=>'function(){$("#book_'.$data->id.'").addClass("loading");}', 'success'=>'function(data) {$("#book_'.$data->id.'").removeClass("loading"); jQuery("#liborder_total").replaceWith(data.total); if (data.status == "OK") {jQuery("#book_'.$data->id.'").replaceWith(data.val); if (data.msg != "") {jQuery("#flash-common-success div").html(data.msg); jQuery("#flash-common-success").dialog({show:"fade", hide:"puff", modal:true, title:"'.Yii::t('app', 'Information').'", autoOpen:true, width:400, minHeight:50, buttons:{"Ok":function(){$(this).dialog("close");}}});}} else {if (data.msg != "") {jQuery("#flash-common-error div").html(data.msg); jQuery("#flash-common-error").dialog({show:"fade", hide:"puff", modal:true, title:"'.Yii::t('app', 'Error').'", autoOpen:true, width:400, minHeight:50, buttons:{"Ok":function(){$(this).dialog("close");}}});}}}'), array('id'=>'submit_book_basic_'.$data->id))?>
+					<?echo CHtml::link(($data->basic->count ? t('Change Order') : t('Add To Order')), array('saveSheet', 'id'=>$data->basic->id), array('class'=>'button libOrder', 'id'=>'submit_book_basic_'.$data->id, 'rel'=>$data->id));?>
 					</form>
 					<?
 				}
@@ -66,7 +64,7 @@
 					<?echo CHtml::hiddenField('LibOrder[book_id]', $data->id)?>
 					<?echo CHtml::hiddenField('LibOrder[type]', 'R')?>
 					<?php echo CHtml::encode($data->reserve->count); ?> <?echo CHtml::textField('LibOrder[count]', $data->reserve->count, array('size'=>'5')); ?> <?echo t('pcs')?>
-					<?php echo CHtml::ajaxSubmitButton(($data->reserve->count ? t('Change Order') : t('Add To Order')), array('saveSheet', 'id'=>$data->reserve->id), array('type'=>'POST', 'dataType'=>'json', 'beforeSend'=>'function(){$("#book_'.$data->id.'").addClass("loading");}', 'success'=>'function(data) {$("#book_'.$data->id.'").removeClass("loading"); jQuery("#liborder_total").replaceWith(data.total); if (data.status == "OK") {jQuery("#book_'.$data->id.'").replaceWith(data.val); if (data.msg != "") {jQuery("#flash-common-success div").html(data.msg); jQuery("#flash-common-success").dialog({show:"fade", hide:"puff", modal:true, title:"'.Yii::t('app', 'Information').'", autoOpen:true, width:400, minHeight:50, buttons:{"Ok":function(){$(this).dialog("close");}}});}} else {if (data.msg != "") {jQuery("#flash-common-error div").html(data.msg); jQuery("#flash-common-error").dialog({show:"fade", hide:"puff", modal:true, title:"'.Yii::t('app', 'Error').'", autoOpen:true, width:400, minHeight:50, buttons:{"Ok":function(){$(this).dialog("close");}}});}}}'), array('id'=>'submit_book_reserve_'.$data->id))?>
+					<?echo CHtml::link(($data->reserve->count ? t('Change Order') : t('Add To Order')), array('saveSheet', 'id'=>$data->reserve->id), array('class'=>'button libOrder', 'id'=>'submit_book_reserve_'.$data->id, 'rel'=>$data->id));?>
 					</form>
 					<?
 				}
--- ck/protected/views/libOrder/sheet.php
+++ ck/protected/views/libOrder/sheet.php
@@ -2,8 +2,6 @@
 $this->pageTitle = Yii::app()->name.' - '.Yii::t('app', 'Order');
 ?>
 
-<?php $this->insertDialog(); ?>
-
 <h1><?php echo Yii::t('app', 'Order'); ?></h1>
 
 <?
@@ -36,3 +34,21 @@ if (user()->library_order_placed)
 )); ?>
 
 </div>
+
+<?php Yii::app()->getClientScript()->registerScript('libOrder_submit', '
+	jQuery(document).off("click", ".libOrder").on("click", ".libOrder", function() {
+		var book_id = jQuery(this).attr("rel");
+		var url = jQuery(this).attr("href");
+		jQuery.ajax({
+			"type":"POST",
+			"dataType":"json",
+			"beforeSend":function() {jQuery("#book_"+book_id).addClass("loading");},
+			"success":function(data) {jQuery("#book_"+book_id).removeClass("loading"); jQuery("#liborder_total").replaceWith(data.total); if (data.status == "OK") {jQuery("#book_"+book_id).replaceWith(data.val); if (data.msg != "") {jQuery("#flash-common-success div").html(data.msg); jQuery("#flash-common-success").dialog({show:"fade", hide:"puff", modal:true, title:"'.Yii::t('app', 'Information').'", autoOpen:true, width:400, minHeight:50, buttons:{"Ok":function(){jQuery(this).dialog("close");}}});}} else {if (data.msg != "") {jQuery("#flash-common-error div").html(data.msg); jQuery("#flash-common-error").dialog({show:"fade", hide:"puff", modal:true, title:"'.Yii::t('app', 'Error').'", autoOpen:true, width:400, minHeight:50, buttons:{"Ok":function(){jQuery(this).dialog("close");}}});}}},
+			"url":url,
+			"cache":false,
+			"data":jQuery(this).parents("form").serialize()
+		});
+		return false;
+	});', CClientScript::POS_READY
+);
+?>
